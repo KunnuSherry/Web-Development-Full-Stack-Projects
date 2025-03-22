5745238535aec0f1fe026fcce615206d1eefa122
KunnuSherry <gamingvenom465@gmail.com>
2025-03-22 11:17:39 +0530

    final changes for deploying

--- KyaSochraHai/models/post.js
+++ KyaSochraHai/models/post.js
@@ -1,6 +1,5 @@
 const mongoose  = require('mongoose');
 
-
 const postSchema = mongoose.Schema({
     user:{
         type: mongoose.Schema.Types.ObjectId,
--- KyaSochraHai/models/users.js
+++ KyaSochraHai/models/users.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 const mongoose  = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/kyasochrahai');
+mongoose.connect(process.env.MONGODB_URI);
 
 const userSchema = mongoose.Schema({
     username : String,
